06ed54d51a0880b7d920327ee309be5d400406f4
Rapheal Odejinmi <77197711+Rapheal555@users.noreply.github.com>
2024-09-20 12:51:11 +0100

    done

--- src/app/diagnose/page.tsx
+++ src/app/diagnose/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Typhoid from "@/components/Test";
+import Test from "@/components/Test";
 import { Container } from "@mantine/core";
 
 export default function Page() {
   return (
     <Container mt="xl">
-      <Typhoid />
+      <Test />
     </Container>
   );
 }
